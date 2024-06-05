32555effd8045294d5ffb441eac5513312290ab3
japhet <mounkassajaphet@gmail.com>
2024-06-05 16:55:52 +0100

    google

--- components/ui/social.tsx
+++ components/ui/social.tsx
@@ -10,7 +10,7 @@ export function Social() {
     return ( 
         <div className=" mt-8 flex w-full">
             {/* <Button className=" flex justify-between p-2 w-full mx-2 shadow-lg rounded-md" variant="ghost" onClick={()=>handleClick("github")}><FaGithub /> <span>Github</span></Button> */}
-            <Button className="p-2 shadow-lg rounded-md" variant="ghost" onClick={()=>handleClick("google")}> <FcGoogle />se connecter avec <span> Google</span></Button>
+            <Button className="flex justify-center items-center p-2 shadow-lg rounded-md w-full space-x-4" variant="ghost" onClick={()=>handleClick("google")}> <FcGoogle /><span><span>se connecter avec </span><span> Google</span></span></Button>
         </div>
     )
 }
